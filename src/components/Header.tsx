6d4bc7f63ac64e4199d4b026e6ede1a28feeecfc
“eddiejoe-antonio” <108960000+eddiejoe-antonio@users.noreply.github.com>
2024-05-16 15:22:28 -0400

    About modal and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { QuestionMarkCircleIcon, MapPinIcon, ListBulletIcon } from '@heroicons/react/24/outline';
 import { useNavigate } from 'react-router-dom';
-import Tooltip from './Tooltip';
+import AboutModal from './AboutModal'; // Import the modal component
 
 interface HeaderProps {
   selectedView: string;
@@ -9,8 +9,8 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ selectedView, setSelectedView }) => {
-  const iconSize = '2em';
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false); // State to manage modal visibility
 
   const handleNavigate = (view: string) => {
     setSelectedView(view);
@@ -18,46 +18,45 @@ const Header: React.FC<HeaderProps> = ({ selectedView, setSelectedView }) => {
   };
 
   const handleAboutClick = () => {
-    setSelectedView('');
-    navigate('/about');
+    setIsModalOpen(true); // Open the modal
   };
 
   return (
-    <div className='w-full bg-white'>
+    <div className='w-full'>
       <div className='block md:grid md:grid-cols-12 items-center'>
         <div className='mt-16 md:mb-12 col-start-2 col-span-8 gap-0 text-left'>
-          <span className="text-[#3B75A9] text-[1.25rem] md:text-[2rem] font-regular font-['Source Sans Pro'] uppercase">
+          <span className="block md:inline text-[#3B75A9] text-[1.5rem] md:text-[2rem] font-regular font-['Source Sans Pro'] uppercase">
             North Carolina{` `}
           </span>
-          <span className="text-[#092940] text-[1.25rem] md:text-[2rem] font-black font-['Source Sans Pro'] uppercase">
+          <span className="block md:inline text-[#092940] text-[1.5rem] md:text-[2rem] font-black font-['Source Sans Pro'] uppercase">
             Digital Equity Resource Finder
           </span>
         </div>
-        <div className='flex justify-start items-center col-start-11 col-span-1 my-2 md:my-0 md:mt-3'>
-          <Tooltip message='I want to learn more about this tool!'>
-            <QuestionMarkCircleIcon
-              className='h-auto tooltip-icon cursor-pointer hover:text-[#1E79C8]'
-              style={{ width: iconSize, height: iconSize }}
-              onClick={handleAboutClick}
-            />
-          </Tooltip>
+        <div className='flex justify-start items-center col-start-10 col-span-2 my-4 md:my-0 md:mt-3'>
+          <button
+            onClick={handleAboutClick}
+            className='flex items-center px-4 py-2 bg-[#FFE9EE] rounded-full shadow-lg cursor-pointer md:hover:bg-[#BC2442] md:hover:text-[#FFE9EE] text-[#BC2442] transition ease-in-out duration-300'
+          >
+            <QuestionMarkCircleIcon className='w-6 h-6 mr-2' />
+            <span className='whitespace-nowrap'>Learn More</span>
+          </button>
         </div>
       </div>
       <div className='grid grid-cols-12 w-full'>
         <div className='col-start-1 col-span-12 md:col-start-2 md:col-span-7 mb-4'>
           <p className=''>
-            The North Carolina Office of Digital Equity and Literacy has gathered digital equity
-            resources across the state. Use this application to filter and view resources in your
-            community, find support across the state, or direct others to the tools they need to
-            thrive online. Help us build a stronger state of digital equity!
+            <strong className=''>Welcome to the Resource Finder!</strong> The North Carolina Office
+            of Digital Equity and Literacy built this interactive tool for exploring the digital
+            equity resources available in North Carolina. Scroll, filter, and click through the
+            results to find support for digital equity needs across North Carolina.{' '}
           </p>
         </div>
         <div className='col-start-1 col-span-12 md:col-start-10 md:col-span-2 flex justify-between md:justify-center'>
           <button
             className={`flex-grow flex items-center justify-center gap-2 py-1 md:h-14 rounded-l-lg ${
               selectedView === 'map'
-                ? 'bg-[#BC2442] hover:bg-[#CF1F42] text-white'
-                : 'bg-[#1E79C8] hover:bg-[#3892E1] text-white'
+                ? 'bg-[#092940] hover:bg-[#092940] text-white transition ease-in-out duration-300'
+                : 'bg-[#1E79C8] hover:bg-[#3892E1] text-white transition ease-in-out duration-300'
             }`}
             onClick={() => handleNavigate('map')}
           >
@@ -67,8 +66,8 @@ const Header: React.FC<HeaderProps> = ({ selectedView, setSelectedView }) => {
           <button
             className={`flex-grow flex items-center justify-center gap-2 py-1 md:h-14 rounded-r-lg ${
               selectedView === 'list'
-                ? 'bg-[#BC2442] hover:bg-[#CF1F42] text-white'
-                : 'bg-[#1E79C8] hover:bg-[#3892E1] text-white'
+                ? 'bg-[#092940] hover:bg-[#092940] text-white transition ease-in-out duration-300'
+                : 'bg-[#1E79C8] hover:bg-[#3892E1] text-white transition ease-in-out duration-300'
             }`}
             onClick={() => handleNavigate('list')}
           >
@@ -77,6 +76,9 @@ const Header: React.FC<HeaderProps> = ({ selectedView, setSelectedView }) => {
           </button>
         </div>
       </div>
+
+      {/* About Modal */}
+      {isModalOpen && <AboutModal onClose={() => setIsModalOpen(false)} />}
     </div>
   );
 };
