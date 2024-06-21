322a11bc678e033f65a8cd5656ff0c170cd40f86
“eddiejoe-antonio” <108960000+eddiejoe-antonio@users.noreply.github.com>
2024-06-21 14:23:00 -0400

    Updated s3 call

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -2,15 +2,21 @@ import Papa from 'papaparse';
 import { Resource } from '../types/resourceFinderTypes';
 
 export const fetchResources = async (): Promise<Resource[]> => {
-  // const response = await fetch('https://files.nc.gov/asset-hub/resources.csv');
-  const response = await fetch('https://nc-resource-finder.s3.amazonaws.com/resources.csv');
+  const response = await fetch('https://files.nc.gov/asset-hub/resources.csv', {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+
   if (!response.ok) {
     throw new Error('Failed to fetch resources');
   }
+
   const csvText = await response.text();
   const parsedData = Papa.parse<Resource>(csvText, {
     header: true,
     dynamicTyping: true,
   }).data;
+
   return parsedData;
 };
