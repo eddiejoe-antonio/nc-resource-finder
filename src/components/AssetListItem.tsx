7e0db39bdb12da3ca6cda23e6acdb3d15fb62157
“eddiejoe-antonio” <108960000+eddiejoe-antonio@users.noreply.github.com>
2024-06-26 11:46:45 -0400

    Fixed wrapping

--- src/components/AssetListItem.tsx
+++ src/components/AssetListItem.tsx
@@ -29,28 +29,28 @@ const AssetListItem: React.FC<AssetListItemProps> = ({ resource }) => {
         <p className='mt-1 font-bold'>{resource.name}</p>
       </div>
       <div className='px-4 pt-4'>
-        <div className='flex flex-wrap overflow-x-auto items-center text-sm px-2 py-2 mb-2 border border-gray-300 rounded-lg'>
+        <div className='flex items-center text-sm px-2 py-2 mb-2 border border-gray-300 rounded-lg'>
           <MapPinIcon className='h-5 w-5 mr-2 text-[#0E3052] flex-shrink-0' />
-          <div className='flex-grow whitespace-normal break-words'>{resource.geography}</div>
+          <div className='overflow-x-auto flex-grow min-w-0'>{resource.geography}</div>
         </div>
       </div>
       <div className='px-4'>
         <div className='flex items-center text-sm px-2 py-2 mb-2 border border-gray-300 rounded-lg'>
           <BookmarkIcon className='h-5 w-5 mr-2 text-[#0E3052] flex-shrink-0' />
-          <div className='flex-grow whitespace-normal break-words'>
+          <div className='flex-grow min-w-0 whitespace-normal break-words'>
             {formatType(resource.primary_type ?? '')}
           </div>
         </div>
       </div>
       <div className='px-4 pb-6'>
-        <div className='flex flex-wrap overflow-x-auto items-center text-sm px-2 py-2 mb-2 border border-gray-300 rounded-lg'>
+        <div className='flex items-center text-sm px-2 py-2 mb-2 border border-gray-300 rounded-lg'>
           <LinkIcon className='h-5 w-5 mr-2 text-[#0E3052] flex-shrink-0' />
           {resource.website && (
             <a
               href={formatWebsite(resource.website)}
               target='_blank'
               rel='noopener noreferrer'
-              className='md:hover:text-[#1E79C8] transition-colors ease-in-out duration-300 flex-grow whitespace-normal break-words'
+              className='md:hover:text-[#1E79C8] transition-colors ease-in-out duration-300 flex-grow min-w-0 whitespace-normal break-words'
             >
               {resource.website}
             </a>
